720298626624c3659a842e3603313e869f4b6b15
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2021-11-02 12:06:05 +0700

    intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,24 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
 import './intro.scss'
 
 const Intro = () => (
   <div className="intro" id="intro">
+    <div className="left">
+      <div className="image-container">
+        <img src="assets/duy.png" />
+      </div>
+    </div>
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi there, I'm</h2>
+        <h1>Le Khanh Duy</h1>
+        <h3>developer<span></span></h3>
+      </div>
+      <a href="#portfolio">
+        <KeyboardArrowDownIcon fontSize={"large"} className="arrow" />
+      </a>
+    </div>
   </div>
 )
 
