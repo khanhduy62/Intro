24f557a992f68033b6f20f07034d82ce3eaca75f
Le Khanh Duy <lekhanhduyduy1201@gmail.com>
2021-11-06 00:01:32 +0700

    typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,16 @@
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+ import { init } from 'ityped'
+import { useEffect, useRef } from 'react';
 
 import './intro.scss'
 
-const Intro = () => (
+const Intro = () => {
+  const textRef = useRef();
+  useEffect(() => {
+   init(textRef.current, { showCursor: true, strings: ['React Native', 'ReactJS' ] })
+  }, [])
+
+  return (
   <div className="intro" id="intro">
     <div className="left">
       <div className="image-container">
@@ -13,7 +21,7 @@ const Intro = () => (
       <div className="wrapper">
         <h2>Hi there, I'm</h2>
         <h1>Le Khanh Duy</h1>
-        <h3>developer<span></span></h3>
+        <h3>Developer: <span ref={textRef}></span></h3>
       </div>
       <a href="#portfolio">
         <KeyboardArrowDownIcon fontSize={"large"} className="arrow" />
@@ -21,5 +29,6 @@ const Intro = () => (
     </div>
   </div>
 )
+} 
 
 export default Intro;
