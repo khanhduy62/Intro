2ec7f3e23fab0e388f18b7925f41a27ae52c4d93
Duy Le <duyle@hmsfirst.com>
2021-11-06 19:52:14 +0700

    portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,55 @@
-import './portfolio.scss'
+import './portfolio.scss';
 
-const Portfolio = () => (
-  <div className="portfolio" id="portfolio">
-  </div>
-)
+const Portfolio = () => {
+  const list = [
+    {
+      id: 'featured',
+      title: 'Featured',
+    },
+    {
+      id: 'web',
+      title: 'Web App',
+    },
+    {
+      id: 'mobile',
+      title: 'Mobile App',
+    },
+    {
+      id: 'design',
+      title: 'Design',
+    },
+    {
+      id: 'content',
+      title: 'Content',
+    },
+  ];
+
+  return (
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Content</li>
+      </ul>
+      <div className='container'>
+        <div className='item'>
+          <img src='assets/app-1.png' alt='icon' />
+          <h3>Ecommerce App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/app-1.png' alt='icon' />
+          <h3>Ecommerce App</h3>
+        </div>
+        <div className='item'>
+          <img src='assets/app-1.png' alt='icon' />
+          <h3>Ecommerce App</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
