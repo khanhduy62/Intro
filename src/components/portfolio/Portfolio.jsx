f9bdd65df07185f8a5c7140671e0818fa9f6966f
Duy Le <duyle@hmsfirst.com>
2021-11-07 16:10:08 +0700

    slider

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -35,15 +35,36 @@ const Portfolio = () => {
         ))}
       </ul>
       <div className='container'>
-        <div className='item'>
+        <div
+          className='item'
+          onClick={() =>
+            window.open(
+              'https://apps.apple.com/us/app/life-in-belgium-utilisateur/id1522459263',
+              '_blank'
+            )
+          }
+        >
           <img src='assets/app-1.png' alt='icon' />
           <h3>Ecommerce App</h3>
         </div>
-        <div className='item'>
+        <div
+          className='item'
+          onClick={() =>
+            window.open(
+              'https://apps.apple.com/vn/app/apple-store/id1568235375',
+              '_blank'
+            )
+          }
+        >
           <img src='assets/app-2.png' alt='icon' />
           <h3>Medication App</h3>
         </div>
-        <div className='item'>
+        <div
+          className='item'
+          onClick={() =>
+            window.open('https://chat.hmsfirst.com:5001', '_blank')
+          }
+        >
           <img src='assets/app-3.png' alt='icon' />
           <h3>Chat App</h3>
         </div>
